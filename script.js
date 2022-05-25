7027842268e39a21b9ea96c5eacb70ed33ac5786
Rob <robertmiller4498@gmail.com>
2022-05-24 22:36:15 -0700

    Saving progress.

--- script.js
+++ script.js
@@ -1,39 +1,83 @@
-const a = 4;
-const b = 2;
+let firstNum = "";
+let secondNum = "";
+let operator = "";
 
-function calcAddition (a, b) {
-    return console.log(a + b);
+function calcAddition (firstNum, secondNum) {
+    return console.log(firstNum + secondNum);
 }
 
-function calcSubtract (a, b) {
-    return console.log(a - b);
+function calcSubtract (firstNum, secondNum) {
+    return console.log(firstNum - secondNum);
 }
 
-function calcMultiply (a, b) {
-    return console.log(a * b);
+function calcMultiply (firstNum, secondNum) {
+    return console.log(firstNum * secondNum);
 }
 
-function calcDivide (a, b) {
-    return console.log(a / b);
+function calcDivide (firstNum, secondNum) {
+    return console.log(firstNum / secondNum);
 }
 
 
-function operate (a, b) {
-    console.log(calcMultiply(a, b));
-}
+let operatorButtons = document.querySelectorAll('.operatorButtons');
+const numbers = document.querySelectorAll(".numberButtons");
 
-operate(a, b);
+ let displayNumber = document.querySelectorAll('button').forEach(button => {
+    button.addEventListener('click', e => {
+        let value = button.value;
+        return document.querySelector('.displayValue').innerText += value;
+        if (operatorButtons === "") {
+            firstNum += e.target.innerText;
+            console.log(firstNum)
+        } else {
+            secondNum += e.target.innerText;
+            console.log(secondNum)
+        }
+        })
+});  
 
+/* numbers.forEach(number => {
+    number.addEventListener("click", e => {
+        if (operator === "") { // Read first number if no operator set yet
+            firstNum += e.target.innerText;
+            console.log(firstNum)
+        } else { // Read second number
+            secondNum += e.target.innerText;
+            console.log(secondNum)
+        }
+    });
+}); */
 
+operatorButtons.forEach(button => {
+    button.addEventListener('click', e => {
+        if (e.target.innerText !== "=") {
+            operator = e.target.innerText;
 
-let buttonFired = document.querySelectorAll('button').forEach(button => {
-    button.addEventListener('click', () => {
-        const value = button.value;
-        return document.querySelector('.displayValue').textContent = value;
-        });
+            console.log(firstNum);
+            console.log(operator);
+
+        } else {
+            console.log(secondNum);
+
+            switch (operator) {
+                case "+":
+                    console.log(calcAddition(firstNum, secondNum));
+                    break;
+
+                case "-":
+                    console.log(calcSubtract(firstNum, secondNum));
+                    break;
+
+                case "*":
+                    console.log(calcMultiply(firstNum, secondNum));
+                    break;
+                
+                case "/":
+                    console.log(calcDivide(firstNum, secondNum));
+                
+                default:
+                    break;
+            }
+        }
+    })
 });
-
-
-
-
-
